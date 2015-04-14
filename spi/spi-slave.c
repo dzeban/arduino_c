ea17cb1f3cc88bf28140237dcc3195d40da2e841
Alex Dzyoba <avd@reduct.ru>
2015-04-14 13:39:25 +0300

    Almost working SPI slave.

---
+++ spi/spi-slave.c
@@ -0,0 +1,46 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+// Initialize SPI slave device
+void spi_init_slave()
+{
+	DDRB = (1 << 6);   // MISO as OUTPUT
+	SPCR = (1 << SPE); // Enable SPI
+}
+
+// Send and receive data. Used in both master and slave.
+unsigned char spi_xfer(unsigned char data)
+{
+	// Load data into buffer
+	SPDR = data;
+
+	// Wait until transmission complete
+	while (!(SPSR & (1 << SPIF)));
+
+	// Return received data
+	return SPDR;
+}
+
+enum {
+	BLINK_DELAY_MS = 1000,
+};
+
+int main()
+{
+	/*spi_init_slave();*/
+
+	/* set pin 5 of PORTB for output*/
+	DDRB |= _BV(DDB5);
+
+	while(1) {
+		/* set pin 5 high to turn led on */
+		PORTB |= _BV(PORTB5);
+		_delay_ms(BLINK_DELAY_MS);
+
+		/* set pin 5 low to turn led off */
+		PORTB &= ~_BV(PORTB5);
+		_delay_ms(BLINK_DELAY_MS);
+	}
+
+}
